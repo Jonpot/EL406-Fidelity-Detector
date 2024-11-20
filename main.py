192dde4a514972399b6eb6a6f7a7b188bfcb6c9e
Jonpot <jonathanpotter18@gmail.com>
2024-11-20 18:26:54 -0500

    Various small updates

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import time
 import requests
 import json
 import cv2
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
 
 def wait_until_fiducial_in_square(x: float, y: float, width: int, height: int) -> bool:
     """
@@ -83,30 +87,49 @@ def check_nozzles(cycles) -> dict[str, list[bool]]:
     return nozzle_status
 
 
-def send_webhook(nozzle_report: dict[str,list[bool]], video_path: str):
+def send_webhook(nozzle_report: dict[str,list[bool]], video_path: str, email: str) -> None:
     """
     Send a webhook containing the nozzle report and the video to a Slack channel.
+    Also sends the video at "output.mjpeg" to the email specified in the email variable.
     
     Args:
         nozzle_report: The nozzle report
         video_path: The path to the video
+        email: The email address to send the video to
     """
+    #slack_webhook_url = 'INSERT'
+    #slack_data = {
+    #    'text': f'Nozzles were detected to be clogged: {json.dumps(nozzle_report)}',
+    #}
 
-    slack_webhook_url = 'INSERT'
-    slack_data = {
-        'text': f'Nozzles were detected to be clogged: {json.dumps(nozzle_report)}',
-    }
+    #response = requests.post(
+    #    slack_webhook_url, data=json.dumps(slack_data),
+    #    headers={'Content-Type': 'application/json'}
+    #)
 
-    response = requests.post(
-        slack_webhook_url, data=json.dumps(slack_data),
-        headers={'Content-Type': 'application/json'}
-    )
+    #if response.status_code != 200:
+    #    print('Request failed with status', response.status_code)
+    #else:
+    #    print('Request successful')
 
-    if response.status_code != 200:
-        print('Request failed with status', response.status_code)
-    else:
-        print('Request successful')
+    # Send the video to the email
+    #msg = MIMEMultipart()
+    #msg['From'] = 'clog@magnify.com'
+    #msg['To'] = email
+    #msg['Subject'] = 'Nozzle Clog Detection Video'
+    
+    #part = MIMEBase('application', 'octet-stream')
+    #with open(video_path, 'rb') as file:
+    #    part.set_payload(file.read())
+    #encoders.encode_base64(part)
+    #part.add_header('Content-Disposition', f'attachment; filename={video_path}')
+    #msg.attach(part)
 
+    #with smtplib.SMTP('smtp.freesmtpservers.com', 25) as server:
+    #    #server.starttls()
+    #    #server.login('clog@magnify.com', "")
+    #    server.sendmail('clog@magnify.com', email, msg.as_string())
+    #    server.quit()
 
 
 def main():
@@ -119,7 +142,7 @@ def main():
     # Take a 10 second video 
     #take_10_second_video('output.mjpeg')
 
-    cycles = threshold_video_movement('videos/varied_flavors/primeSA2.mjpeg')
+    cycles = threshold_video_movement('output.mjpeg')
 
     # Check if the nozzles are clogged
     nozzle_report = check_nozzles(cycles)
@@ -135,7 +158,7 @@ def main():
     # Send the nozzle report to a webhook along with video if nozzles are clogged
     if any(nozzle_report.get('front', [])) or any(nozzle_report.get('back', [])):
         print("Nozzles are clogged, sending report to webhook.")
-        #send_webhook(nozzle_report, 'output.mjpeg')
+        send_webhook(nozzle_report, 'output.mjpeg', "jonathanpotter18@gmail.com")
     else:
         print("Nozzles are not clogged, no action needed.")
 
