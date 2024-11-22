535a993f904858b41d1da6114a3997cf266d61e5
Jonpot <jonathanpotter18@gmail.com>
2024-11-22 08:54:19 -0500

    Cleans up and removes old unused functions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from detect_clogs import threshold_video_movement, detect_fiducials, homography, classify_nozzles
+from detect_clogs import threshold_video_movement, homography, classify_nozzles
 import time
 #from picamzero import Camera
 import requests
@@ -8,43 +8,6 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
-
-def wait_until_fiducial_in_square(x: float, y: float, width: int, height: int) -> bool:
-    """
-    Waits until the fiducial is within the square defined by the top left corner (x, y) and the width and height
-    of the square. Returns True if the fiducial is within the square, False if 10 seconds pass without the fiducial
-    being within the square.
-    
-    Args:
-        x: x coordinate of the top left corner of the square
-        y: y coordinate of the top left corner of the square
-        width: width of the square
-        height: height of the square
-        
-    Returns:
-        True if the fiducial is within the square, False otherwise
-    """
-    #cam = Camera()
-    #start_time = time.time()
-    #while time.time() - start_time < 10:
-    #    #frame = cam.capture()
-    #    fiducial_coordinate = detect_fiducial(frame)
-    #    if x < fiducial_coordinate[0] < x + width and y < fiducial_coordinate[1] < y + height:
-    #        return True
-    #    time.sleep(0.1)
-    return False
-    
-def take_10_second_video(output_path: str) -> None:
-    """
-    Takes a 10 second video and saves it to the output path.
-    
-    Args:
-        output_path: The path to save the video to
-    """
-    cam = Camera()
-    cam.start_recording(output_path)
-    time.sleep(10)
-    cam.stop_recording()
 
 def check_nozzles(cycles) -> dict[str, list[bool]]:
     """
